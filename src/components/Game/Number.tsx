27fd448342d7eee42d344e5ad0bdb0fb5f887138
kid-us <kidushh29@gmail.com>
2024-07-30 21:48:02 +0300

    Card layout changed

--- src/components/Game/Number.tsx
+++ src/components/Game/Number.tsx
@@ -10,7 +10,7 @@ const Number = ({ number, calledNum, markedNumbers, onMark }: Props) => {
     "bg-red-700 text-white border border-gray-600 py-[5px]";
 
   const otherStyles =
-    "mb-1 rounded-md text-center font-poppins text-[20px] font-extrabold shadow shadow-zinc-400 cursor-pointer lg:p-[14px] py-[7px] px-[4px] m-[3px] ";
+    "mb-1 rounded-md text-center font-poppins text-[20px] font-extrabold shadow shadow-zinc-400 cursor-pointer lg:p-[14px] py-[13px] px-[13px] m-[3px] ";
 
   // Handle Mark
   const handleMark = (num: number) => {

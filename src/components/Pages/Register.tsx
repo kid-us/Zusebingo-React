91a6217f673b5746b6626b76e70edf11412c92f4
kid-us <kidushh29@gmail.com>
2024-08-06 01:21:19 +0300

    Padding

--- src/components/Pages/Register.tsx
+++ src/components/Pages/Register.tsx
@@ -94,7 +94,7 @@ const Register = () => {
   return (
     <div className="bg3 pb-14">
       <div className="container mx-auto flex justify-center align-middle h-auto pt-14">
-        <div className="lg:w-[38%] lg:px-2 px-4">
+        <div className="lg:w-[38%] px-2">
           <div className="flex justify-center">
             <img src={logo} alt="Logo" className="w-28" />
           </div>

6c60a5a1e70ab082f1e125213e715235779fb9fc
kid-us <kidushh29@gmail.com>
2024-08-06 17:36:37 +0300

    Socket url changed

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-// const socket = io("https://socket.zusebingo.com");
-const socket = io("ws://7.tcp.eu.ngrok.io:12783");
+const socket = io("https://socket.zusebingo.com");
+// const socket = io("ws://7.tcp.eu.ngrok.io:12783");
 
 export default socket;
